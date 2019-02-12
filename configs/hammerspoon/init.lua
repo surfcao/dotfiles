5fed1da2b993547aa83b3019c8758f4bac4052ec
Guofeng Cao <surfcao@gmail.com>
2019-02-12 11:10:47 -0600

    update

---
+++ configs/hammerspoon/init.lua
@@ -0,0 +1,18 @@
+vim = hs.loadSpoon('VimMode')
+
+-- Basic key binding to ctrl+;
+-- You can choose any key binding you want here, see:
+--   https://www.hammerspoon.org/docs/hs.hotkey.html#bind
+
+hs.hotkey.bind({'ctrl'}, ';', function()
+  vim:enter()
+end)
+
+vim:enableKeySequence('j', 'k')
+
+vim:disableForApp('iTerm2')
+vim:disableForApp('MacVim')
+vim:disableForApp('Terminal')
+vim:disableForApp('VIM')
+vim:disableForApp('Vivaldi')
+vim:disableForApp('Emacs')
