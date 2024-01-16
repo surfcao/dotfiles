c4f109568a20922dce69e4011dc0ebf90581f13f
Guofeng Cao <surfcao@gmail.com>
2024-01-16 12:29:54 -0700

    update

--- configs/emacs.d/init.el
+++ configs/emacs.d/init.el
@@ -538,14 +538,19 @@
   :config
   (setq-default highlight-symbol-idle-delay 1.5))
 
+(use-package magit-popup
+  :ensure t
+  :defer t
+  :config 
+   (magit-define-popup-switch 'magit-log-popup ?f "first parent" "--first-parent"))
+
 (use-package magit
   :ensure t
   :defer t
   :config
   (setq magit-branch-arguments nil)
   (setq magit-push-always-verify nil)
-  (setq magit-last-seen-setup-instructions "1.4.0")
-  (magit-define-popup-switch 'magit-log-popup ?f "first parent" "--first-parent"))
+  (setq magit-last-seen-setup-instructions "1.4.0"))
 
 ;(use-package mmm-mode
 ;  :ensure t
